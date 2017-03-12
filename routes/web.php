2d0f3d32c2c51a54be7b87adda379c1dcf362d53
just <g.staikov85@gmail.com>
2017-03-12 04:00:34 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+$app->get('/login', $callback);
+$app->post('/login', $callback);
+$app->get('/login', $callback);
+$app->put('/save', $callback);
+$app->patch($uri, $callback);
+$app->delete($uri, $callback);
